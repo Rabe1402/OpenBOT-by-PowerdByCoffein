dbe0386ced1700abcc36f692b6acada4d5b12a74
Rabe1402 <huberrabe@gmail.com>
2025-05-21 11:52:54 +0200

    just formating

--- FollowLineBeta/FTL/FTL.ino
+++ FollowLineBeta/FTL/FTL.ino
@@ -44,51 +44,57 @@ void loop()
   // read 8 Bytes from OpenMV END
  
   //--- Button logic -----
-if (READ_BUTTON_CLOSED(B1)==1) {
+  if (READ_BUTTON_CLOSED(B1)==1) 
+  {
     start_normal = 1;
     WRITE_LCD_CLEAR(); 
-}
+  }
 
-if (READ_BUTTON_CLOSED(B4)==1) {
+  if (READ_BUTTON_CLOSED(B4)==1) 
+  {
     start_normal = 0; 
     WRITE_LCD_CLEAR();
     WRITE_MOTOR(ML, 0);
     WRITE_MOTOR(MR, 0);
-}
+  }
 
-if (start_normal==0) {
-  Serial.println("no program selected");
-  Serial.println("press button B1 for normal startup");
-  WRITE_LCD_TEXT(1, 1, "Select Program");
-  WRITE_LCD_TEXT(1, 2, "B1 for normal startup");
-  delay(100); 
-}
+  if (start_normal==0) 
+  {
+    Serial.println("no program selected");
+    Serial.println("press button B1 for normal startup");
+    WRITE_LCD_TEXT(1, 1, "Select Program");
+    WRITE_LCD_TEXT(1, 2, "B1 for normal startup");
+    delay(100); 
+  }
 
-if (start_normal==1) {
-  line_error = spi_buffer[1] - 100;
-  motorl_speed = motorl_offset - kp * line_error;
-  motorr_speed = motorr_offset + kp * line_error;
+  if (start_normal==1) 
+  {
+   line_error = spi_buffer[1] - 100;
+    motorl_speed = motorl_offset - kp * line_error;
+    motorr_speed = motorr_offset + kp * line_error;
  
-  WRITE_MOTOR(ML,(int)motorl_speed);
-  WRITE_MOTOR(MR,(int)motorr_speed);
+    WRITE_MOTOR(ML,(int)motorl_speed);
+    WRITE_MOTOR(MR,(int)motorr_speed);
  
-  WRITE_LCD_TEXT(1,1,"Angle:");
-  WRITE_LCD_INT(7,2,line_error,4);
+    WRITE_LCD_TEXT(1,1,"Angle:");
+    WRITE_LCD_INT(7,2,line_error,4);
  
-  WRITE_LCD_TEXT(3,4,"ML");
-  WRITE_LCD_INT(1,5,(int)motorl_speed,4);
+    WRITE_LCD_TEXT(3,4,"ML");
+    WRITE_LCD_INT(1,5,(int)motorl_speed,4);
  
-  WRITE_LCD_TEXT(12,4,"MR");
-  WRITE_LCD_INT(10,5,(int)motorr_speed,4);
+    WRITE_LCD_TEXT(12,4,"MR");
+    WRITE_LCD_INT(10,5,(int)motorr_speed,4);
   
-  WRITE_LCD_INT(1, 6, (int)spi_buffer[2], 4);
+    WRITE_LCD_INT(1, 6, (int)spi_buffer[2], 4);
 
-  while (spi_buffer[2]==1) {
-   WRITE_MOTOR(ML, 0);
-   WRITE_MOTOR(MR, 0);
-   WRITE_LCD_CLEAR(); 
-   WRITE_LCD_TEXT(1, 1, "RED LINE");
-   WRITE_LCD_TEXT(1, 2, "END OF CODE");
-  } 
+    while (spi_buffer[2]==1) 
+    {
+      WRITE_MOTOR(ML, 0);
+      WRITE_MOTOR(MR, 0);
+      WRITE_LCD_CLEAR(); 
+      WRITE_LCD_TEXT(1, 1, "RED LINE");
+      WRITE_LCD_TEXT(1, 2, "END OF CODE");
+    } 
+  }
 }
-}
+
